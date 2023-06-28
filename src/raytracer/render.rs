f1697c3d7f8eb117876b978883c621c00de907e0
JohnEdrickGador <jmgador@up.edu.ph>
2023-06-28 23:18:01 +0800

    refactor: adjusted make_ray and render functions in render.rs

--- src/raytracer/render.rs
+++ src/raytracer/render.rs
@@ -7,14 +7,16 @@ use rayon::prelude::*;
 use std::sync::Mutex;
 
 fn make_ray(scene: &Scene, pixel_coords: (usize, usize)) -> Ray {
-    // Create coordinate frame
     let w = (scene.camera.eye - &scene.camera.center).norm();
     let u = scene.camera.up.cross(&w).norm();
     let v = w.cross(&u);
 
     let fov_y_rad = scene.camera.fovy.to_radians();
-    let weight_a = ((0.5 * fov_y_rad).tan() / (0.5 * (scene.img_height as f64))) * (((pixel_coords.1 as f64) + 0.5) - (0.5 * (scene.img_width as f64)));
-    let weight_b = ((0.5 * fov_y_rad).tan() / (0.5 * (scene.img_height as f64))) * ((0.5 * (scene.img_height as f64)) - (0.5 + (pixel_coords.0 as f64)));
+    let half_img_height = scene.img_height as f64 * 0.5;
+    let half_img_width = scene.img_width as f64 * 0.5;
+
+    let weight_a = (0.5 * fov_y_rad.tan() / half_img_height) * ((pixel_coords.1 as f64 + 0.5) - half_img_width);
+    let weight_b = (0.5 * fov_y_rad.tan() / half_img_height) * (half_img_height - (0.5 + pixel_coords.0 as f64));
 
     let ray_dir = (u * weight_a + &(v * weight_b) - &w).norm();
 
@@ -24,44 +26,36 @@ fn make_ray(scene: &Scene, pixel_coords: (usize, usize)) -> Ray {
     }
 }
 
-pub fn render(scene: &Scene) -> Vec <u8> {
-    let mut pixels: Vec <u8> = Vec::new();
-    pixels.resize(scene.img_width * scene.img_height * 3, 0u8);
-
+pub fn render(scene: &Scene) -> Vec<u8> {
+    let pixels: Vec<u8> = vec![0u8; scene.img_width * scene.img_height * 3];
     let pixels_mutex = Mutex::new(pixels);
 
     (0..scene.img_height).into_par_iter().for_each(|i| {
         (0..scene.img_width).into_par_iter().for_each(|j| {
-            // Pass through ray in center of (i, j) pixel
             let ray = make_ray(scene, (i, j));
             let start_idx = (i * scene.img_width + j) * 3;
 
-            // Intersection test with scene
             if let Some(id) = intersect_scene_from_view(ray, scene) {
-                // Use get_color_recursive to get reflections
-                //let pix_color = get_color(ray, scene, id, &scene.lights);
-                let mut pixels = pixels_mutex.lock().unwrap();
-
                 let pix_color = get_color_recursive(ray, &scene, id, 0);
-                
-                pixels[start_idx + 0] = (255.0 * pix_color[0]) as u8;
-                pixels[start_idx + 1] = (255.0 * pix_color[1]) as u8;
-                pixels[start_idx + 2] = (255.0 * pix_color[2]) as u8;
-            }
-            else {
-                // Color all pixels black
                 let mut pixels = pixels_mutex.lock().unwrap();
+                let pixels_slice = &mut pixels[start_idx..start_idx + 3];
 
-                pixels[start_idx + 0] = 0u8;
-                pixels[start_idx + 1] = 0u8;
-                pixels[start_idx + 2] = 0u8;
+                pixels_slice.copy_from_slice(&[
+                    (255.0 * pix_color[0]) as u8,
+                    (255.0 * pix_color[1]) as u8,
+                    (255.0 * pix_color[2]) as u8,
+                ]);
+            } else {
+                let mut pixels = pixels_mutex.lock().unwrap();
+                let pixels_slice = &mut pixels[start_idx..start_idx + 3];
+                pixels_slice.copy_from_slice(&[0u8, 0u8, 0u8]);
             }
         });
     });
 
-    let pixels = pixels_mutex.lock().unwrap().to_owned();
-    pixels
+    pixels_mutex.into_inner().unwrap()
 }
+
 
 pub fn build_image(image_dim: (usize, usize), pixels: &Vec <u8>) -> DynamicImage {
     if pixels.len() % 3 != 0 {
